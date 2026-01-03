392702991e24fab0dfd780bb40af2f48455d8dfc
Jeremyapass <jeremyapascal@gmail.com>
2026-01-03 19:51:28 +0700

    REFACTOR(layout): restructure root layout architecture

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import TanstackProvider from "@/provider/tanstackProvider";
 import Footer from "@/components/Molecules/Footer";
 import { fonts } from "@/fonts/fonts";
+import { RootProvider } from "@/provider/rootProvider";
 
 export const metadata = {
   title: "Movian",
@@ -13,11 +14,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`antialiased bg-[#0D0D0D] w-full min-h-screen text-white flex flex-col items-center justify-center pt-[134px] px-[64px] ${fonts.satoshi.className}`}
+        className={`antialiased bg-[#0D0D0D] min-h-screen text-white ${fonts.satoshi.className}`}
       >
         <TanstackProvider>
-          <Navbar />
-          {children}
+          <RootProvider>
+            <main className="flex flex-col items-center justify-center pt-[134px] px-[64px] w-full">
+              <Navbar />
+              {children}
+            </main>
+          </RootProvider>
+
           <Footer />
         </TanstackProvider>
       </body>
