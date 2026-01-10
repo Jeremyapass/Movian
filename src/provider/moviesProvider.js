9c4f9b823079c6b2e9dc56167aa2442e982a23aa
Jeremyapass <jeremyapascal@gmail.com>
2026-01-10 22:15:57 +0700

    FEAT(providers): Add pagination handling to movies provider

--- src/provider/moviesProvider.js
+++ src/provider/moviesProvider.js
@@ -5,27 +5,40 @@ import { UseGetPopular } from "@/hookAPI/TMDB/movieList/UseGetPopular";
 import { UseGetTopRated } from "@/hookAPI/TMDB/movieList/UseGetTopRated";
 import { UseGetUpcoming } from "@/hookAPI/TMDB/movieList/UseGetUpcoming";
 import { useParams, useRouter } from "next/navigation";
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 const MoviesContext = createContext();
 
 export const MoviesProvider = ({ children }) => {
   const route = useRouter();
   const { movieCategory } = useParams();
+  const [currentPage, setCurrentPage] = useState(1);
+
   const { data: nowPlayingData, isLoading: isNowPlayingLoading } =
-    UseGetNowPlaying(!movieCategory || movieCategory === "now-playing");
+    UseGetNowPlaying(
+      !movieCategory || movieCategory === "now-playing",
+      currentPage
+    );
   const { data: popularData, isLoading: isPopularLoading } = UseGetPopular(
-    !movieCategory || movieCategory === "popular"
+    !movieCategory || movieCategory === "popular",
+    currentPage
   );
   const { data: upcomingData, isLoading: isUpcomingLoading } = UseGetUpcoming(
-    !movieCategory || movieCategory === "upcoming"
+    !movieCategory || movieCategory === "upcoming",
+    currentPage
   );
   const { data: topRatedData, isLoading: isTopRatedLoading } = UseGetTopRated(
-    !movieCategory || movieCategory === "top-rated"
+    !movieCategory || movieCategory === "top-rated",
+    currentPage
   );
 
   const handleViewAllClick = (category) => {
     route.push(`/movies/${category}`);
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
@@ -41,6 +54,8 @@ export const MoviesProvider = ({ children }) => {
         isUpcomingLoading,
         isTopRatedLoading,
 
+        currentPage,
+        handlePageChange,
         handleViewAllClick,
       }}
     >
