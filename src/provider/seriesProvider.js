a264b6adb322d66f89d4f834beabb7ae118b76f3
Jeremyapass <jeremyapascal@gmail.com>
2026-01-10 22:16:26 +0700

    FEAT(providers): Add pagination handling to series provider

--- src/provider/seriesProvider.js
+++ src/provider/seriesProvider.js
@@ -5,28 +5,46 @@ import { UseGetOnTheAirSeries } from "@/hookAPI/TMDB/seriesList/UseGetOnTheAirSe
 import { UseGetPopularSeries } from "@/hookAPI/TMDB/seriesList/UseGetPopularSeries";
 import { UseGetTopRatedSeries } from "@/hookAPI/TMDB/seriesList/UseTopRatedSeries";
 import { useParams, useRouter } from "next/navigation";
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 const SeriesContext = createContext();
 
 export const SeriesProvider = ({ children }) => {
   const route = useRouter();
   const { seriesCategory } = useParams();
+  const [currentPage, setCurrentPage] = useState(1);
 
   const { data: airingTodaySeriesData, isLoading: isAiringTodayLoading } =
-    UseGetAiringTodaySeries(!seriesCategory || seriesCategory === "airing-today");
+    UseGetAiringTodaySeries(
+      !seriesCategory || seriesCategory === "airing-today",
+      currentPage
+    );
 
   const { data: onTheAirData, isLoading: isOnTheAirLoading } =
-    UseGetOnTheAirSeries(!seriesCategory || seriesCategory === "on-the-air");
+    UseGetOnTheAirSeries(
+      !seriesCategory || seriesCategory === "on-the-air",
+      currentPage
+    );
 
   const { data: popularData, isLoading: isPopularLoading } =
-    UseGetPopularSeries(!seriesCategory || seriesCategory === "popular");
+    UseGetPopularSeries(
+      !seriesCategory || seriesCategory === "popular",
+      currentPage
+    );
 
   const { data: topRatedData, isLoading: isTopRatedLoading } =
-    UseGetTopRatedSeries(!seriesCategory || seriesCategory === "top-rated");
+    UseGetTopRatedSeries(
+      !seriesCategory || seriesCategory === "top-rated",
+      currentPage
+    );
 
   const handleViewAllClick = (category) => {
     route.push(`/series/${category}`);
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
@@ -42,6 +60,8 @@ export const SeriesProvider = ({ children }) => {
         isPopularLoading,
         isTopRatedLoading,
 
+        currentPage,
+        handlePageChange,
         handleViewAllClick,
       }}
     >
