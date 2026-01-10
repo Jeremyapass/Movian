539f91f9fc6292b9e0af4db3f9eba72fab7a0bb2
Jeremyapass <jeremyapascal@gmail.com>
2026-01-10 22:02:01 +0700

    REFACTOR(carousel): Update favorite carousel with movieCacheId prop

--- src/components/Molecules/carouselLayout/FavoriteCarouselLayout.jsx
+++ src/components/Molecules/carouselLayout/FavoriteCarouselLayout.jsx
@@ -107,6 +107,7 @@ const FavoriteCarouselLayout = ({ title, data, isLoading }) => {
             return (
               <Card
                 key={item.id}
+                movieCacheId={item.movie_cache_id}
                 filmName={item.movie_cache.name}
                 filmReleaseDate={item.movie_cache.date_release}
                 filmImages={`https://image.tmdb.org/t/p/w500${item.movie_cache.poster_path}`}
