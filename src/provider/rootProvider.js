555bbc62abd0418e5b4e878e3335cd3dd08a432f
Jeremyapass <jeremyapascal@gmail.com>
2026-01-10 22:16:10 +0700

    FEAT(providers): Add toast notifications to root provider

--- src/provider/rootProvider.js
+++ src/provider/rootProvider.js
@@ -1,12 +1,14 @@
 "use client";
 import { useGetAccountDetail } from "@/hookAPI/SUPABASE/publicSchema/account/useGetAccountDetail";
 import React, { createContext, useContext } from "react";
+import { ToastContainer } from "react-toastify";
 
 const RootContext = createContext();
 
 export const RootProvider = ({ children }) => {
   const { data: getAccountDetailData, isLoading: isGetAccountDetailLoading } =
     useGetAccountDetail();
+
   return (
     <RootContext.Provider
       value={{
@@ -15,6 +17,18 @@ export const RootProvider = ({ children }) => {
       }}
     >
       {children}
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </RootContext.Provider>
   );
 };
