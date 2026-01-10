b1d769172d2f8a9f3e15c21bc57b878bf65cddb8
Jeremyapass <jeremyapascal@gmail.com>
2026-01-10 21:45:46 +0700

    FEAT(movies): Add pagination support to movie list endpoints

--- src/app/api/TMDB/movieList/get-now-playing/route.js
+++ src/app/api/TMDB/movieList/get-now-playing/route.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 export const GET = (req) => {
+  const { searchParams } = new URL(req.url);
+  const page = searchParams.get("page") || "1";
+
   return axios
     .get(`${process.env.API_URL_TMDB}/movie/now_playing`, {
       params: {
         api_key: process.env.API_KEY_TMDB,
         language: "en-US",
-        page: 1,
+        page: page,
       },
     })
     .then((res) => {
