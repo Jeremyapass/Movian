07d7c1f0d01726db84b6a544d04fed769b332460
Jeremyapass <jeremyapascal@gmail.com>
2026-01-10 21:46:38 +0700

    FEAT(series): Add pagination support to series list endpoints

--- src/app/api/TMDB/seriesList/get-airing-today-series/route.js
+++ src/app/api/TMDB/seriesList/get-airing-today-series/route.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 export const GET = (req) => {
+  const { searchParams } = new URL(req.url);
+  const page = searchParams.get("page") || "1";
+
   return axios
     .get(`${process.env.API_URL_TMDB}/tv/airing_today`, {
       params: {
         api_key: process.env.API_KEY_TMDB,
         language: "en-US",
-        page: 1,
+        page: page,
       },
     })
     .then((res) => {
@@ -21,4 +24,4 @@ export const GET = (req) => {
         headers: { "Content-Type": "application/json" },
       });
     });
-}
+};
